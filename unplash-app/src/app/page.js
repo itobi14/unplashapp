23d89e53936a96864bbe14d552dac4bf45a950c1
Tobi <tobi.kok@hva.nl>
2024-05-17 16:06:48 +0200

    photographers and photos set-up

--- unplash-app/src/app/page.js
+++ unplash-app/src/app/page.js
@@ -2,42 +2,57 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+      <main className={styles.main}>
 
-        <div className={styles.heroSection}>
-            <img src="/backgroundImg.jpg" className={styles.backgroundImg} alt="Background Img"/>
+          <div className={styles.heroSection}>
+              <img src="/backgroundImg.jpg" className={styles.backgroundImg} alt="Background Img"/>
 
-            <div className={styles.navbar}>
-                <ul className={styles.ul}>
-                    <li className={styles.navItem}>About</li>
-                    <li className={`${styles.navItem} ${styles.active}`}>Discover</li>
-                    <h2 className={styles.logo}>Viewr</h2>
-                    <li className={styles.navItem}>Users</li>
-                    <li className={styles.navItem}>Account</li>
-                </ul>
-            </div>
+              <div className={styles.navbar}>
+                  <ul className={styles.ul}>
+                      <li className={styles.navItem}>About</li>
+                      <li className={`${styles.navItem} ${styles.active}`}>Discover</li>
+                      <h2 className={styles.logo}>Viewr</h2>
+                      <li className={styles.navItem}>Users</li>
+                      <li className={styles.navItem}>Account</li>
+                  </ul>
+              </div>
 
-            <div className={styles.heroTitleContainer}>
-                <h2 className={styles.heroTitle}>Discover the perfect image for your design</h2>
-                <div className={styles.descriptionContainer}>
-                    <div className={styles.line}></div>
-                    <p className={styles.descriptionText}>Photographed by:</p>
-                    <p className={styles.descriptionTextBold}>&nbsp; Maarten Bouwkamp</p>
-                </div>
-            </div>
+              <div className={styles.heroTitleContainer}>
+                  <h2 className={styles.heroTitle}>Discover the perfect image for your design</h2>
+                  <div className={styles.descriptionContainer}>
+                      <div className={styles.line}></div>
+                      <p className={styles.descriptionText}>Photographed by:</p>
+                      <p className={styles.descriptionTextBold}>&nbsp; Maarten Bouwkamp</p>
+                  </div>
+              </div>
 
-            <div className={styles.searchBarContainer}>
-                <div className={styles.searchWrapper}>
-                    <input type="search" className={styles.searchBar}/>
-                    <button className={styles.searchBtn}>Search</button>
-                </div>
-            </div>
-        </div>
+              <div className={styles.searchBarContainer}>
+                  <div className={styles.searchWrapper}>
+                      <input type="search" className={styles.searchBar}/>
+                      <button className={styles.searchBtn}>Search</button>
+                  </div>
+              </div>
+          </div>
 
-        <div className={styles.photographersSection}>
+          <div className={styles.photographersSection}>
 
-        </div>
+              <h2 className={styles.photographersTitle}>Photographers</h2>
+              <div className={styles.photographersContainer}>
+                  <div className={styles.photographerCard}></div>
+                  <div className={styles.photographerCard}></div>
+                  <div className={styles.photographerCard}></div>
+                  <div className={styles.photographerCard}></div>
+              </div>
+          </div>
 
-    </main>
+          <div className={styles.photosSection}>
+
+              <h2 className={styles.photosTitle}>Photos</h2>
+              <div className={styles.photosContainer}>
+
+              </div>
+          </div>
+
+      </main>
   );
 }
